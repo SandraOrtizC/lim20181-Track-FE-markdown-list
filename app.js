f36acf4bd1d0b31a0203adbb2623f3cd51c6aab2
sandraortiz <rocio12vx@gmail.com>
2018-08-21 18:17:57 -0500

    archivo markdown a html

--- app.js
+++ app.js
@@ -1,28 +1,16 @@
-// libreria recorre el archivo y los filtra por extension 
-// let klaw = require('klaw'),
-// path = require('path'),
-// through2 = require('through2'),
-// dir_walk = process.argv[2] || process.cwd();
 
-
-// klaw(dir_walk)
-
-// .pipe(through2.obj(function (item, enc, next) {
-
-//         let ext = path.extname(item.path);
-
-//         if (ext.toLowerCase() === '.md' || ext.toLowerCase() === '.md') {
-
-//             this.push(item);
-
-//         }
-
-//         next();
-
-//     }))
-
-// .on('data', function (item) {
-
-//     console.log(item.path);
-
-// });
+let klaw = require('klaw'),
+path = require('path'),
+through2 = require('through2'),
+dir_walk = process.argv[2] || process.cwd();
+klaw(dir_walk)
+.pipe(through2.obj(function (item, enc, next) {
+        let ext = path.extname(item.path);
+        if (ext.toLowerCase() === '.md' || ext.toLowerCase() === '.md') {
+            this.push(item);
+        }
+        next();
+    }))
+.on('data', function (item) {
+    console.log(item.path);
+})
--- index.js
+++ index.js
@@ -1,30 +1,59 @@
 #!/usr/bin/env node
 const fs = require('fs');
 const [, , ...args] = process.argv
+var showdown = require('showdown')
 const path = require('path');
-const filemdvalidate = () => {
-    if (path.extname(`${args}`) == '.md') {
+
+
+    if (path.extname(`${args}`) === '.md') {
         const mdfile = () => {
-            // console.log(`${args}`);
             fs.readFile(args[0], 'utf8', (err, data) => {
                 if (err) {
                     console.log(err);
-                } else {
-                    console.log(data.toString());
+                }
+                else {
+                    converter = new showdown.Converter()
+                    html = converter.makeHtml(data);
+                    console.log(html);
                 }
             });
         }
         mdfile();
-// 
     }
     else {
         console.log('el archivo no es markdown');
     }
 
-}
-filemdvalidate()
 
 
+
+
+
+
+
+    
+  // const filemdvalidate = () => {  
+// }
+// filemdvalidate()
+
+
+
+
+
+
+
+
+                    // console.log(html)
+                    // console.log(typeof(html));
+                    // var anchor = document.getElementById("guia");
+                    // var hola  = document.getElementsByTagName("a")[0].getAttribute("href"); 
+                    //  var href_val = html.split("href"); 
+                    // console.log(href_val);
+
+                    // var href_val =href_val.split// Si no funciona proba: var href_val =  href_val.split("\#");
+
+                    // var ancla_actual = href_val[href_val.length-1];
+                    //  console.log(href_val)
 
 
 
