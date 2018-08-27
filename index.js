bfce87bd0d35c5cf69679866dbe74fb89e2dd895
sandraortiz <rocio12vx@gmail.com>
2018-08-27 09:15:27 -0500

    closes #1 , closes #5

--- index.js
+++ index.js
@@ -2,8 +2,33 @@
 const fs = require('fs');
 const [, , ...args] = process.argv
 const path = require('path');
-const paths = args[0]
+const route = args[0]
 const filemd = [];
+// const fileordirectory = () => {
+//   fs.stat(paths, (err, stat) => {
+//     if (stat.isFile()) {
+//       readFile(paths)
+//     }
+//     else if (stat.isDirectory()) {
+//       console.log('es una carpeta')
+//       readFolder(paths);
+//     }
+//   })
+// }
+const mdlinks = () => {
+  return new Promise((resolve, reject) => {
+    fs.stat(route, (err, stat) => {
+      if (stat.isFile()) {
+        readFile(route)
+      }
+      else {
+        readFolder(route)
+      }
+    })
+  })
+ }
+mdlinks()
+module.exports = mdlinks
 const readFolder = (currentPath) => {
   const files = fs.readdirSync(currentPath);
   for (let i in files) {
@@ -21,32 +46,23 @@ const readFile = (currentFile) => {
   if (path.extname(currentFile) === '.md') {
     const fileMd = path.basename(currentFile)
     filemd.push(fileMd)
-    console.log(filemd);
+    filemd.map(file => readfiles(file))
   }
-
 }
-const fileordirectory = () => {
-  fs.stat(paths, (err, stat) => {
-    if (stat.isFile()) {
-      readFile(paths)
+const readfiles = (files) => {
+  fs.readFile(files, 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
     }
-    else if (stat.isDirectory()) {
-      console.log('es una carpeta')
-      readFolder(paths);
+    else {
+      let result = data.match(/(?:__|[])|\[(.*?)\]\(.*?\)/gm);
+      console.log(result);
     }
-  })
+  });
 }
-const extraerlinks = () => { }
-const mdlinks = () => {
-  if (path.isAbsolute(paths)) {
-    fileordirectory()
-  }
-  else {
-  fileordirectory()
-  }
 
-}
-mdlinks()
+
+
 
 
 
