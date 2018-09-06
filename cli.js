3ece2ba51dc3057c544226ec0df52e3ad853a506
sandraortiz <rocio12vx@gmail.com>
2018-09-06 18:28:41 -0500

    closes #15 , closes #16 , closes #17

--- cli.js
+++ cli.js
@@ -1,18 +1,49 @@
 #!/usr/bin/env node
-const mdlinks = require('./index');
-const program = require('commander');
-let options = {
-	validate: false,
-	stats: false
-}
-program
-	.arguments('<path>')
-	.option('-v, --validate', 'check validate link')
-	.option('-s, --stats', 'Information link')
-	.action((route) => {
-		mdlinks(route, options).then(data => {
-			console.log(data);
 
-		})
-	})
-.parse(process.argv);
+const program = require('commander');
+const mdLinks = require('./index.js')
+
+let options = {
+    validate: false,
+    stats: false
+}
+
+program
+    .arguments('<path>')
+    .option('-v, --validate', 'Validar links si estan rotos o no')
+    .option('-s, --stats', 'Mostrar stats de los links')
+    .action((route) => {
+        options.validate = program.validate;
+        options.stats = program.stats;
+        mdLinks(route, options)
+            .then(array => {
+                if (!options.stats && !options.validate) {
+                    array.forEach(linksData => {
+                        console.log(`${linksData.file}\t ${linksData.href}\t ${linksData.text}`);
+                    });
+                }
+
+                else if (options.stats && options.validate){
+                    array.forEach(linksData => {
+                        console.log(`total:${linksData.total}\nunique:${linksData.unique}\nbroken:${linksData.broken}`)
+                   });
+                }
+               else if(options.validate == true) {
+                    array.forEach(linksData => {
+                        console.log(`${linksData.file}\t ${linksData.href}\t ${linksData.status}\t${linksData.statusText}`);
+                    });
+                }
+               else if(options.stats == true ) {
+                    array.forEach(linksData => {
+                         console.log(`total:${linksData.total}\nunique:${linksData.unique}`)
+                    });
+                }
+                
+            
+            })
+    })
+    .parse(process.argv);
+
+
+
+
--- index.js
+++ index.js
@@ -2,82 +2,89 @@ const fs = require('fs');
 const path = require('path');
 const marked = require('marked');
 const fetch = require('node-fetch');
+const arrayUniques = require('array-unique');
 
-const uniqueLinks = (links) => {
-    return [...new Set(links)];
+const validarStatus = (arrLinks) => {
+  const arrayDePromises = arrLinks.map(link => fetch(link));
+  return Promise.all(arrayDePromises)
+    .then(response => {
+      const links = arrLinks.map((objRespuesta, statuslink) => {
+        objRespuesta.status = response[statuslink].status;
+        objRespuesta.statusText = response[statuslink].statusText;
+        return objRespuesta;
+      });
+      return links;
+    })
 }
-const validarStatus = (links) => {
-    return fetch(links.href)
-        .then(response => {
-            links.status = res.status;
-            links.statusText = res.statusText;
-            return links
-        })
-        .catch(err => {
-            links.codeError = err.code;
-            return links
-        })
-}
- const linksInfo = []
-const linkContent = (routeAbsolute) => {
 
-    const datalink = fs.readFileSync(routeAbsolute, 'utf8');
+const searchLink = (resultado) => {
+  const links = [];
+  resultado.forEach(file => {
+    const data = fs.readFileSync(file, 'utf8');
     const renderer = new marked.Renderer();
-    renderer.link = (href, route, text) => {
-        linksInfo.push({ href, text, route: routeAbsolute });
+    renderer.link = (href, title, text) => {
+      links.push({
+        href: href,
+        text: text,
+        file: file
+      });
     };
-    marked(datalink, { renderer });
-    return linksInfo
-    
+    marked(data, { renderer });
+  })
+  return links;
 }
-
-
-
-
-const checkFileMd = (file) => {
-    if (path.extname(file) === '.md') {
-        return file;
-    }
-};
 
 const readFileOrDirectory = (routeAbsolute) => {
-    const stats = fs.statSync(routeAbsolute)
-    if (stats.isFile()) {
-        if (checkFileMd(routeAbsolute)) {
-            linkContent(routeAbsolute)
-        }
+  let arrfiles = [];
+  const stats = fs.statSync(routeAbsolute)
+  if (stats.isFile() && path.extname(routeAbsolute) === '.md') {
+    arrfiles.push(routeAbsolute);
+  }
+  else if (stats.isDirectory()) {
+    const files = fs.readdirSync(routeAbsolute);
+    files.forEach(file => {
+      arrfiles = arrfiles.concat(readFileOrDirectory(path.join(routeAbsolute, file)));
+    });
+  }
+  return arrfiles;
+}
+
+const mdLinks = (route, options) => {
+  return new Promise((resolve, reject) => {
+    if (fs.existsSync(route)) {
+      const routaAbsolute = path.resolve(route)
+      const read = readFileOrDirectory(routaAbsolute)
+      const array = searchLink(read)
+
+      if (!options.stats && !options.validate) {
+        resolve(array)
+      }
+      else if (options.stats && options.validate) {
+        validarStatus(array)
+          .then((respuesta) => {
+            resolve([{
+              total: respuesta.map(link => link.href).length,
+              broken: respuesta.filter(link => link.status == 404).length,
+              unique: arrayUniques(respuesta.map(link => link.href)).length
+            }])
+
+          });
+      }
+      else if (options.validate === true) {
+        validarStatus(array)
+          .then((respuesta) => { resolve(respuesta) });
+      }
+      else if (options.stats === true) {
+        validarStatus(array)
+          .then((respuesta) => {
+            resolve([{
+              total: respuesta.map(link => link.href).length,
+              unique: arrayUniques(respuesta.map(link => link.href)).length
+            }])
+          });
+      }
     }
-    else if (stats.isDirectory()) {
-        let files = fs.readdirSync(routeAbsolute);
-        files.map((file) => {
-            readFileOrDirectory(routeAbsolute + '/' + file);
-        });
-    }
+  })
 }
 
-
-
-const mdlinks = (route, options) => {
-    return new Promise((resolve, reject) => { 
-       options.validate = false
-       options.stats = false
-  if (fs.existsSync(route)) {
-      const routeAbsolute = path.resolve(route)
-      if (!options.validate && !options.stats) {
-        readFileOrDirectory (routeAbsolute)
-          linksInfo.map(link => {
-            console.log(`${link.route}\t${link.href}\tLink a\t${link.text}\r\n`);
-           })
-          }
-        else if(options.validate === true && options.stats===undefined) {
-            readFileOrDirectory (routeAbsolute)
-            linksInfo.map(link => {
-              console.log(`${link.route}\t${link.href}`);
-             })
-        }
-
-}
-})
-}
-
-module.exports = mdlinks;
+module.exports = mdLinks;
